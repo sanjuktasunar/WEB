49d47251aba5a354fbef92258e1686ada50b51f0
sanjuktasunar <misshean5@gmail.com>
2021-05-04 10:08:09 +0545

    layout-design

--- web/web/Controllers/PublicSite/HomeController.cs
+++ web/web/Controllers/PublicSite/HomeController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using web.Utility;
 
 namespace web.Controllers.PublicSite
 {
     public class HomeController : Controller
     {
+        public DisplayParamters _displayParameters = new DisplayParamters();
         public ActionResult Index()
         {
-            return View();
+            var obj = _displayParameters.GetParameters();
+            obj.MenuLink = _displayParameters.GetMenuLink();
+            return View(obj);
         }
     }
 }
---
+++ web/web/Utility/ParameterClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web.Utility
+{
+    public class ParameterClass
+    {
+        public string Home { get; set; }
+        public string AboutUs { get; set; }
+        public string ContactUs { get; set; }
+        public string Login { get; set; }
+
+        public MenuLink MenuLink { get; set; }
+    }
+
+    public class MenuLink
+    {
+        public string HomeLink { get; set; }
+        public string AboutUsLink { get; set; }
+        public string ContactUsLink { get; set; }
+        public string LoginLink { get; set; }
+    }
+}
