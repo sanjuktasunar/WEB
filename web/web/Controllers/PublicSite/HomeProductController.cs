884da4b1af7839b3dc87f073a3dc52164c2edeea
sanjuktasunar <misshean5@gmail.com>
2021-05-09 19:14:01 +0545

    bin and cache

---
+++ web/web/Controllers/PublicSite/HomeProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.Controllers.PublicSite
+{
+    public class HomeProductController : Controller
+    {
+        [Route("~/Products")]
+        public ActionResult Product()
+        {
+            return View();
+        }
+    }
+}
