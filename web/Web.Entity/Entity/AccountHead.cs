ddeb4f10be83f8932455f9891f9b1a865a797800
sanjuktasunar <misshean5@gmail.com>
2021-06-01 15:42:32 +0545

    main branch

---
+++ web/Web.Entity/Entity/AccountHead.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Entity.Entity
+{
+    public class AccountHead
+    {
+        public int AccountHeadId { get; set; }
+        public string AccountHeadName { get; set; }
+        public string AccountNumber { get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; }
+    }
+}
