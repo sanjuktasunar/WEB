3390c1d65186cea8bfdd9ca65584753738d35d3d
sanjuktasunar <misshean5@gmail.com>
2021-06-01 19:24:36 +0545

    changes

---
+++ web/Web.Services/Services/Account/AccountHeadService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Services.Services.Account
+{
+    public interface IAccountHeadService
+    {
+
+    }
+
+    public class AccountHeadService:IAccountHeadService
+    {
+
+    }
+}
